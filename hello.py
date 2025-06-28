46c277d6875b0970105776adb81e2c7fd6fa7fe2
sujoybarua-ctr <sujoybinary@gmail.com>
2025-06-28 05:19:47 -0700

    python beggining

---
+++ hello.py
@@ -0,0 +1,10 @@
+print("hello!")
+python_number = 15
+print(python_number)
+is_ok = True
+cgpa = "is 4"
+message = """ 
+hi hello ,
+im not what you think of me
+"""
+print(message)
---
+++ random.cpp
@@ -0,0 +1,22 @@
+//oop for id password input
+#include<iostream>
+using namespace std;
+class inf
+{
+private:
+string pass;
+string u;
+public:
+ void set(string usn){
+    usn=u;
+    
+ }
+};
+int main()
+{inf o; string usn;
+cout<<"enter username =";
+ 
+cin>>usn;
+
+return 0;
+}
